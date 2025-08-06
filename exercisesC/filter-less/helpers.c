e57917b943688606e770fa858c8462d30339ec81
enzoconsulo <enzoconsulo@gmail.com>
2025-08-06 19:58:11 -0300

    Memory exercise Added blur filter to filter exercise

--- exercisesC/filter-less/helpers.c
+++ exercisesC/filter-less/helpers.c
@@ -7,9 +7,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0 ; i < height ; i++){//for each pixel in image, convert to a average of his colors (to provide tons between white and black)
         for(int j = 0 ; j < width ; j++){
 
-                                  //using 16 bits instead of "byte" to previne overflow
+                                  //using 16 bits instead of "BYTE" to previne overflow
             uint16_t average = 0; // for example, if all rgb was 255(11111111) sum all of then will pass the 8 bits from "BYTE"
-                                  //so we use 16 bits to previne, considerer that the maximum is 765(1011111101) with 10 bits (all values 255).
+                                  //so we use 16 bits to previne, considerer that the maximum is 765(1011111101) with 10 bits (all 3 values as 255).
 
             average += image[i][j].rgbtBlue;
             average += image[i][j].rgbtGreen;    //sum into average all colors of this pixel
@@ -50,5 +50,87 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0 ; i < height ; i++){//for each pixel in image, convert to a average of his adjacents (and his) values
+        for(int j = 0 ; j < width ; j++){
+
+            //looklike the grayscale logic, but instead of used same average to every color, each color has his own changes:
+
+            uint16_t averageB = 0; //using 16 bits instead of "BYTE" to previne overflow
+            uint16_t averageG = 0; // for example, if all rgb was 255(11111111) sum all of then will pass the 8 bits from "BYTE"
+            uint16_t averageR = 0; //so we use 16 bits to previne, considerer that the maximum is 2.295(100011110111) with 12 bits (all 9 values as 255).
+
+
+            int usedAdjacents = 0; //to save the number of used adjacents to use in average calculation
+
+            //actual term
+            //       x   y   z
+            //       a  (#)  b          "#" is the actual term
+            //       c   d   e
+            averageB += image[i][j].rgbtBlue;
+            averageG += image[i][j].rgbtGreen;
+            averageR += image[i][j].rgbtRed;
+            usedAdjacents++;
+
+
+            //Three Adjacents Above
+            //      (x) (y) (z)
+            //       a   #   b          "#" is the actual term
+            //       c   d   e
+            for(int k = -1; k <= 1 ; k++){
+                if( (i-1 >= 0) && (j+k >= 0) && (j+k < width)){
+                    averageB += image[i-1][j+k].rgbtBlue;
+                    averageG += image[i-1][j+k].rgbtGreen;
+                    averageR += image[i-1][j+k].rgbtRed;
+                    usedAdjacents++;
+                }
+            }
+
+            //Three Adjacents Under
+            //       x   y   z
+            //       a   #   b          "#" is the actual term [i][j]
+            //      (c) (d) (e)
+            for(int k = -1; k <= 1 ; k++){
+                if( (i+1 < height) && (j+k >= 0) && (j+k < width)){
+                    averageB += image[i+1][j+k].rgbtBlue;
+                    averageG += image[i+1][j+k].rgbtGreen;
+                    averageR += image[i+1][j+k].rgbtRed;
+                    usedAdjacents++;
+                }
+             }
+
+
+            //left side
+            //       x   y   z
+            //      (a)  #   b          "#" is the actual term [i][j]
+            //       c   d   e
+             if((j-1 >= 0)){
+                    averageB += image[i][j-1].rgbtBlue;
+                    averageG += image[i][j-1].rgbtGreen;
+                    averageR += image[i][j-1].rgbtRed;
+                    usedAdjacents++;
+            }
+
+            //right side
+            //       x   y   z
+            //       a   #  (b)          "#" is the actual term [i][j]
+            //       c   d   e
+             if((j+1 < width)){
+                    averageB += image[i][j+1].rgbtBlue;
+                    averageG += image[i][j+1].rgbtGreen;
+                    averageR += image[i][j+1].rgbtRed;
+                    usedAdjacents++;
+            }
+
+
+            averageB /= usedAdjacents;
+            averageG /= usedAdjacents;      //get the average with usedAdjacents (the ones who was valids)
+            averageR /= usedAdjacents;
+
+            image[i][j].rgbtBlue = averageB;
+            image[i][j].rgbtGreen = averageG;     // set the color to the actual term
+            image[i][j].rgbtRed = averageR;
+        }
+    }
+
     return;
 }
