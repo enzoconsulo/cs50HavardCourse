0bd8614ab8ceccc1aeb91c5100694e3b92ae4aa4
enzoconsulo <enzoconsulo@gmail.com>
2025-08-11 22:39:34 -0300

    Data Structure speller implements 'load' function

--- exercisesC/speller/dictionary.c
+++ exercisesC/speller/dictionary.c
@@ -29,14 +29,50 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    return (word[0]) - 'a'; //initial model just to see( ordering by alphabet index 0-26)
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    return false;
+    FILE *input = fopen(dictionary , "r");
+    if(input == NULL){                            //reads the dictionary file. passed in parameter
+        return false;                             //if had an error, return false to indicate a failed "load"
+    }
+
+    char word[LENGTH + 1];                        //declares a buffer to store readed words
+
+    int indexLetter;                              //declare here to use in all the elements on loop
+
+    while(fscanf(input, "%s", word) != EOF){      //read the file until find "EOF" : end of file. word by word(LENGTH+1)
+
+        node *n = malloc(sizeof(node));           //allocates a new node in memory
+        if(n == NULL){                            //if had an error, return false to indicate a failed "load"
+
+            fclose(input);                        //remenbering to close if "break" the loop and don't reach the fclose in the end
+            return false;
+        }
+
+        strcpy(n->word, word);                    //copy the string in word to word field in node "n"
+
+        indexLetter = hash(word);                 //apply the hashcode function
+
+        if(table[indexLetter] == NULL){
+            n->next = NULL;                       //if is the first element in this table[index] add it and set his "next" field to NULL
+            table[indexLetter] = n;
+
+        }else{                                    //if wasn't the first element in this table[index]
+
+            n->next = table[indexLetter];         //set "next" field on node to actual element index on table
+            table[indexLetter] = n;               //set the table[index] pointer to "n" node
+
+        }
+
+    }
+
+    fclose(input);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
