f5298d07455a45dc615ec08d9cd312bb5971dc93
enzoconsulo <enzoconsulo@gmail.com>
2025-09-05 13:50:04 -0300

    Flask Exercise birthdays

--- exercisesFlask/birthdays/app.py
+++ exercisesFlask/birthdays/app.py
@@ -27,13 +27,18 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        
+        db.execute("INSERT INTO birthdays (name,month,day) VALUES (?,?,?)",name,month,day)
 
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
+        query = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html",birthdays = query)
 
 
