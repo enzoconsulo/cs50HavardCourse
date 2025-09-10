05e3695db27ca10d3bafd776347d59ea64177cb2
enzoconsulo <enzoconsulo@gmail.com>
2025-09-10 15:38:31 -0300

    Exercise finance base from statement

--- exercisesFlask/birthdays/static/index.js
+++
@@ -1,4 +0,0 @@
-//                                           TODO
-
-// action litener to verify if name is string, month is between 1 and 12 , adn verify if day is between 1 and 30.
-// if any of these was false, show a alert on top and dont submit the form
